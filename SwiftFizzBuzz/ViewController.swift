ddef7eeb7a8ce778a3487614e5e2393b8d4c5014
John Dyer <jbdyer@gmail.com>
2015-05-18 14:27:32 +0100

    Working program

--- SwiftFizzBuzz/ViewController.swift
+++ SwiftFizzBuzz/ViewController.swift
@@ -10,16 +10,105 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    let startAlert = UIAlertController(title: "New Game", message: "Fizz if divisable by 3.", preferredStyle: UIAlertControllerStyle.Alert)
+    let endAlert = UIAlertController(title: "Wrong choice", message: "You lost the game please try again", preferredStyle: UIAlertControllerStyle.Alert)
+    var currentNum = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        numberLabel.text = currentNum.description
+        startAlert.addAction(UIAlertAction(title: "Begin", style: UIAlertActionStyle.Default,handler: nil))
+        endAlert.addAction(UIAlertAction(title: "Start again", style: UIAlertActionStyle.Default,handler: nil))
+        self.presentViewController(startAlert, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
+    
+    @IBAction func fizzButton(sender: AnyObject)
+    {
+        updateGame("fizz")
+    }
+    @IBAction func buzzButton(sender: AnyObject)
+    {
+        updateGame("buzz")
+    }
+    @IBAction func fizzBuzzButton(sender: AnyObject)
+    {
+        updateGame("fizzbuzz")
+    }
+    @IBAction func nextButton(sender: AnyObject)
+    {
+        updateGame("none")
+    }
+    func updateGame(fizzBuzz: String)
+    {
+        var answerCorrect: Bool = checkAnswer(fizzBuzz)
+        
+        if (answerCorrect)
+        {
+            currentNum++
+            numberLabel.text = currentNum.description
+        }
+        else
+        {
+            self.presentViewController(endAlert, animated: true, completion: nil)
+            currentNum = 1
+            numberLabel.text = currentNum.description
+            self.presentViewController(startAlert, animated: true, completion: nil)
+        }
+    }
+    
+    func checkAnswer(answer: String) -> Bool
+    {
+        switch(answer)
+        {
+        case "fizz":
+            if(currentNum%3==0)
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+        case "buzz":
+            if(currentNum%5==0)
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+        case "fizzbuzz":
+            if(((currentNum%3)==0)&&((currentNum%5)==0))
+            {
+                return true
+            }
+            else
+            {
+                return false
+            }
+        case "none":
+            if(((currentNum%3)>0)&&((currentNum%5)>0))
+            {
+                return true
+            }
+        default:
+            return false
+            
+        }
+        return false
+    }
+    
 
 }
 
