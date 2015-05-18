d84376c04d2cda0a561ef822ef9eb6f1e11362bb
John Dyer <jbdyer@gmail.com>
2015-05-18 14:57:08 +0100

    Commenting and neatening things up

--- SwiftFizzBuzz/ViewController.swift
+++ SwiftFizzBuzz/ViewController.swift
@@ -11,23 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var numberLabel: UILabel!
-    
+    //Set up the two alerts
     let startAlert = UIAlertController(title: "New Game", message: "Fizz if divisable by 3. Buzz if divisable by 5. Fizz buzz if divisable by both.",preferredStyle: UIAlertControllerStyle.Alert)
-    
     let endAlert = UIAlertController(title: "Wrong choice", message: "You lost the game please try again", preferredStyle: UIAlertControllerStyle.Alert)
-    
+    //Number that is currently in the view
     var currentNum = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         numberLabel.text = currentNum.description
-        
-        endAlert.addAction(UIAlertAction(title: "Start again", style: UIAlertActionStyle.Default,handler: nil))
-        
+        //Add action to the alerts
         startAlert.addAction(UIAlertAction(title: "Begin", style: UIAlertActionStyle.Default,handler: nil))
-        
+        endAlert.addAction(UIAlertAction(title: "Start again", style: UIAlertActionStyle.Default,handler: nil))
     }
-    override func viewDidAppear(animated: Bool) {
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        //Show alert at begining of game
         self.presentViewController(startAlert, animated: true, completion: nil)
     }
 
@@ -36,9 +36,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
-
-    
+    //The buttons simply send a varible to updateGame with what was selected
     @IBAction func fizzButton(sender: AnyObject)
     {
         updateGame("fizz")
@@ -55,6 +53,8 @@ class ViewController: UIViewController {
     {
         updateGame("none")
     }
+
+    //Calls checkAnswer() and updates view or restarts game
     func updateGame(fizzBuzz: String)
     {
         var answerCorrect: Bool = checkAnswer(fizzBuzz)
@@ -71,13 +71,14 @@ class ViewController: UIViewController {
             numberLabel.text = currentNum.description
         }
     }
-    
+
+    //Checks mod to check if divisable by 3/5 or both. Returns true if correct answer.
     func checkAnswer(answer: String) -> Bool
     {
         switch(answer)
         {
         case "fizz":
-            if(currentNum%3==0)
+            if currentNum%3==0
             {
                 return true
             }
@@ -86,7 +87,7 @@ class ViewController: UIViewController {
                 return false
             }
         case "buzz":
-            if(currentNum%5==0)
+            if currentNum%5==0
             {
                 return true
             }
@@ -95,7 +96,7 @@ class ViewController: UIViewController {
                 return false
             }
         case "fizzbuzz":
-            if(((currentNum%3)==0)&&((currentNum%5)==0))
+            if ((currentNum%3)==0)&&((currentNum%5)==0)
             {
                 return true
             }
@@ -104,17 +105,15 @@ class ViewController: UIViewController {
                 return false
             }
         case "none":
-            if(((currentNum%3)>0)&&((currentNum%5)>0))
+            //Slight issue of not being able to use != but greater than works.
+            if ((currentNum%3)>0)&&((currentNum%5)>0)
             {
                 return true
             }
         default:
             return false
-            
         }
         return false
     }
-    
-
 }
 
