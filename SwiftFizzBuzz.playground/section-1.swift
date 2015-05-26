0bf56649047854faf89ae305eb35a136b1dbee5a
John Dyer <jbdyer@gmail.com>
2015-05-26 15:20:41 +0100

    Add playground

---
+++ SwiftFizzBuzz.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+for var i = 1; i < 101; i++
+{
+    if ((i%3==0) && (i%5==0))
+    {
+        println("Fizz Buzz")
+    }
+    else if i%3==0
+    {
+        println("Fizz")
+    }
+    else if i%5==0
+    {
+        println("Buzz")
+    }
+    else
+    {
+        println(i)
+    }
+}
